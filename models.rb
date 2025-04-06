79143ceb4e59d43d5b35e9a59d638ee961a993d4
EC2 Default User <ec2-user@ip-10-1-13-100.ap-northeast-1.compute.internal>
2025-04-06 10:58:51 +0000

    remove activerecord as not needed3

--- models.rb
+++ models.rb
@@ -1,12 +1,12 @@
 require 'bundler/setup'
 Bundler.require
 
-ActiveRecord::Base.establish_connection
+# ActiveRecord::Base.establish_connection
 
 # タイムゾーンの設定
-Time.zone = 'Tokyo'
-ActiveRecord::Base.default_timezone = :local
+# Time.zone = 'Tokyo'
+# ActiveRecord::Base.default_timezone = :local
 
-class Post < ActiveRecord::Base
+# class Post < ActiveRecord::Base
     
-end
+# end
